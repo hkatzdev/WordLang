e370f5f663a5dbc8ef021cd11ce7dca6db4d0db8
WilliamRagstad <41281398+WilliamRagstad@users.noreply.github.com>
2019-12-28 01:22:18 +0100

    Added input support

---
+++ WordLang/Interpreter.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace WordLang
+{
+    public class Interpreter
+    {
+        private readonly string sourceFile;
+
+        private Dictionary<string, int> backPoints;
+        private Dictionary<string, double> variables;
+
+        public Interpreter(string sourceFile)
+        {
+            this.sourceFile = sourceFile;
+
+            backPoints = new Dictionary<string, int>();
+            variables = new Dictionary<string, double>();
+        }
+
+        public void Interpret()
+        {
+            string source = File.ReadAllText(sourceFile);
+
+            double DataValue = 0;
+
+            string ct = string.Empty;       // Current Token
+
+            int line = 1;
+            int column = 0;
+
+            int incrementData = 1; // 1 or -1
+
+            bool isComment = false;
+            bool isStoreVariable = false;
+            bool isReadVariable = false;
+
+            for (int i = 0; i < source.Length; i++)
+            {
+                char cc = source[i];
+                if (cc == '\r') continue;
+
+                column++;
+
+                if (cc == ' ' || cc == '\t' || cc == '\n')
+                {
+                    if (cc == '\n') { line++; column = 0; }
+
+                    if (isStoreVariable)
+                    {
+                        variables[ct] = DataValue;
+                        DataValue = 0;
+                        isStoreVariable = false;
+                    }
+                    
+                    continue;
+                }
+                else if (cc == '\'')
+                {
+                    if (isReadVariable)
+                    {
+                        if (variables.ContainsKey(ct)) DataValue += variables[ct];
+                        else
+                        {
+                            Console.WriteLine($"\nError: The variable {ct} is undefined!");
+                        }
+                    }
+
+                    isReadVariable = !isReadVariable;
+
+                    ct = string.Empty;
+                    continue;
+                }
+                else if (cc == '"')
+                {
+                    isComment = !isComment;
+
+                    ct = string.Empty;
+                    continue;
+                }
+
+                ct += cc;
+
+                if (cc == ',')
+                {
+                    incrementData *= -1; // Toggle from 1 -> -1 or vise versa.
+                    ct = string.Empty;
+                }
+                else if (cc == '.')
+                {
+                    Console.Write(getChar(DataValue));
+                    ct = string.Empty;
+                    DataValue = 0;
+                }
+                else if (cc == '<')
+                {
+                    char input = Console.ReadKey().KeyChar;
+                    DataValue += incrementData * input;
+                    ct = string.Empty;
+                }
+                else if (cc == '>')
+                {
+                    isStoreVariable = true;
+                    ct = string.Empty;
+                }
+                else if (cc == '?')
+                {
+                    // Show debug
+                    Console.WriteLine($"\nData value at line {line}, column {column} is '{getChar(DataValue)}'={getAsciiValue(DataValue)} ({DataValue})");
+                }
+                else
+                {
+                    // Normal character
+                    if (!isStoreVariable && !isReadVariable && !isComment) DataValue += incrementData * cc;
+                }
+            }
+        }
+
+        private static int ASCII_SIZE = 255;
+        /// <summary>
+        /// Get the corresponding character in ASCII spectrum
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        private char getChar(double data)
+        {
+            return (char)( getAsciiValue(data) % ASCII_SIZE );
+        }
+
+        private double getAsciiValue(double data)
+        {
+            while (data < 0) data += ASCII_SIZE;
+            return data % ASCII_SIZE;
+        }
+    }
+}
--- WordLang/Program.cs
+++ WordLang/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ArgumentsUtil;
+using System;
+using System.IO;
 
 namespace WordLang
 {
@@ -6,7 +8,49 @@ namespace WordLang
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            args = new[] { "test.wl" };
+
+            Arguments a = Arguments.Parse(args);
+
+            if (a.Keyless.Count > 0)
+            {
+                bool debug = a.ContainsKey("d");
+
+                for (int i = 0; i < a.Keyless.Count; i++)
+                {
+                    if (File.Exists(a.Keyless[i]))
+                    {
+                        if (a[i].ToLower().EndsWith(".wl") || a[i].ToLower().EndsWith(".w"))
+                        {
+                            new Interpreter(a[i]).Interpret();
+
+                            Console.ReadLine(); // Pause
+                        }
+                        else Console.WriteLine($"The file: '{a[i]}' is not a WordLang file!");
+                    }
+                    else Console.WriteLine($"The file: '{a[i]}' was not found!");
+                }
+            }
+            else
+            {
+                ArgumentsTemplate at = new ArgumentsTemplate(
+                    new System.Collections.Generic.List<ArgumentOption>
+                    {
+                        new ArgumentOption("d", "debug", "Enable debugging.")
+                    },
+                    false,
+                    new System.Collections.Generic.List<ArgumentCommand>
+                    {
+                        new ArgumentCommand("[files]", "Files to interpret.")
+                    },
+                    true,
+                    null,
+                    null,
+                    (char)KeySelector.CrossPlatformCompatible
+                );
+
+                at.ShowManual(HelpFormatting.TitleUnderlines);
+            }
         }
     }
 }
